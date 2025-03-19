b4b2edb2599593133cc0f4d5fc6548a93c446d75
Jonatas <djonatas31@gmail.com>
2025-03-19 10:55:44 -0300

    ✨ add alert component

---
+++ src/components/AlertComponent.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { CheckCircle, XCircle } from "lucide-vue-next";
+
+const props = defineProps({
+  message: String,
+  statusCode: Number,
+});
+
+const isError = props.statusCode !== 200;
+</script>
+
+<template>
+  <Alert
+    :class="
+      isError
+        ? 'bg-red-100 border-red-500 text-red-800'
+        : 'bg-green-100 border-green-500 text-green-800'
+    "
+  >
+    <component :is="isError ? XCircle : CheckCircle" class="h-4 w-4" />
+    <AlertTitle>{{ isError ? "Erro!" : "Sucesso!" }}</AlertTitle>
+    <AlertDescription>
+      {{ message }}
+    </AlertDescription>
+  </Alert>
+</template>
