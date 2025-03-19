68076eab49b485c8d38a36fc3ba6f30c0e41f5b2
Jonatas <djonatas31@gmail.com>
2025-03-19 11:18:03 -0300

    📝 fix: title center

--- src/components/TitleComponent.vue
+++ src/components/TitleComponent.vue
@@ -1,12 +1,10 @@
 <template>
   <header>
-    <div
-      class="flex justify-around w-100 items-center mt-2 select-none sm:mx-5 md:mx-5"
-    >
+    <div class="flex justify-around w-100 items-center mt-2 select-none sm:mx-5 md:mx-5">
       <a href="https://www.rainhadapaz.g12.br/" target="_blank">
         <img src="../assets/logo.png" alt="logo-rainha" srcset="" width="150" />
       </a>
-      <h1 class="text-2xl text-primary font-medium">MANHÃ COM EGRESSOS</h1>
+      <h1 class="text-center text-2xl text-primary font-medium">MANHÃ COM EGRESSOS</h1>
     </div>
     <div class="px-5 text-justify py-8 space-y-1">
       <p>É com alegria que esse ano fazemos 75 anos de história!</p>
